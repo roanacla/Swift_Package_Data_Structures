9f4a426a564388cc51f6b583f3ff3f20c276a71a
roanacla <roanacla@me.com>
2020-10-14 22:25:59 -0700

    BinaryTree, Second Version

--- Tests/Swift_Package_Data_StructuresTests/BinaryNodeTests.swift
+++ Tests/Swift_Package_Data_StructuresTests/BinaryNodeTests.swift
@@ -10,16 +10,14 @@ import XCTest
 
 final class BinaryNodesTests: XCTestCase {
   
-  var tree: BinaryNode<Int> = {
-    let zero = BinaryNode(value: 0)
-    let one = BinaryNode(value: 1)
-    let five = BinaryNode(value: 5)
-    let seven = BinaryNode(value: 7)
-    let eight = BinaryNode(value: 8)
-    let nine = BinaryNode(value: 9)
-    //   7
-    // 1   9
-    //0 5 8
+  var tree: BinaryNodeTwo<Int> = {
+    let zero = BinaryNodeTwo(value: 0)
+    let one = BinaryNodeTwo(value: 1)
+    let five = BinaryNodeTwo(value: 5)
+    let seven = BinaryNodeTwo(value: 7)
+    let eight = BinaryNodeTwo(value: 8)
+    let nine = BinaryNodeTwo(value: 9)
+    
     seven.leftChild = one
     one.leftChild = zero
     one.rightChild = five
@@ -29,43 +27,17 @@ final class BinaryNodesTests: XCTestCase {
     return seven
   }()
   
-  func deserialize<T>(array: inout [T?]) -> BinaryNode<T>? {
-    //[nil, 0, nil, 1, nil, 5, nil, 7, nil, 8, nil, 9, nil]
-    
-    guard let value = array.removeFirst() else { return nil}
-    let node = BinaryNode(value: value)
-    node.leftChild = deserialize(array: &array)
-    node.rightChild = deserialize(array: &array)
-    return node
-  }
   
-  func deserialize<T>(array: [T?]) -> BinaryNode<T>? {
-    var array = Array(array.reversed())
-    return deserialize(array: &array)
-  }
-  
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
-    XCTAssertEqual(Swift_Package_Data_Structures().text, "Hello, World!")
-  }
-  
-  func test1() {
-    XCTAssertEqual(tree.leftChild!.leftChild!.value as Int, 0)
-  }
-  
-  func test2() {
-    var array: [Int?] = []
-    let result: [Int?] = [nil, 0, nil, 1, nil, 5, nil, 7, nil, 8, nil, 9, nil]
-    tree.traverseInOrder(visit: { array.append($0) })
-    print(array)
-    XCTAssertEqual(result, array)
+  func testTraverseInOrder() {
+    var output:[Int] = []
+    traverseInOrder(node: tree, visit: { output.append($0)})
+    let answer = [0,1,5,7,8,9]
+    XCTAssertEqual(output, answer)
+    let answerTwo = [0,1,5,7,8,19]
+    XCTAssertNotEqual(output, answerTwo)
   }
   
   static var allTests = [
-    ("testExample", testExample),
-    ("test1", test1),
-    ("test2", test2)
+    ("testTraverseInOrder", testTraverseInOrder)
   ]
 }
